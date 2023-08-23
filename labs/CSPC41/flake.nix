3c8a09d281518b4511b4952c09e747dab31ab836
GetPsyched <priyanshutr@proton.me>
2023-08-23 17:29:12 +0530

    feat: setup ML lab environment

---
+++ labs/CSPC41/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Dev environment for Machine Learning";
+
+  inputs = {
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          nativeBuildInputs = with pkgs; [
+            jupyter
+            (python3.withPackages (p: with p; [ notebook pandas ]))
+          ];
+        };
+      }        
+    );
+}
