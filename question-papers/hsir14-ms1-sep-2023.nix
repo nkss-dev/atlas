4aa9127a3b1cb0b628d36a734e6c73e83a647df8
GetPsyched <priyanshutr@proton.me>
2023-10-02 16:24:33 +0530

    qp: add HSIR14

---
+++ question-papers/hsir14-ms1-sep-2023.nix
@@ -0,0 +1,42 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 1 EXAMINATION";
+
+  date = { day = 5; month = "Sep"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Business Management";
+  course.code = "HSIR14";
+
+  semester = 7;
+  pages = 1;
+  time = "50 minutes";
+
+  questions = [
+    {
+      contents = [{
+        text = ''
+          Mr. X is a middle level manager. He keeps all his subordinates under a lot of discipline. His employees, however, complain of wastage of time and efforts as they feel that nothing is being assigned in a proper way and a proper place; no proper schedule is made for working.
+
+          Which principle of Henry Fayol has been violated by him? What will be the consequences of the violation of this principle?
+        '';
+      }];
+      marks = 3;
+    }
+    {
+      contents = [{
+        text = ''
+          Examine the following statements critically:
+            - a. Management is an open system approach.
+            - b. Gang Plank in Scalar Chain.
+        '';
+      }];
+      marks = 6;
+    }
+    {
+      contents = [{
+        text = "Explain the process of planning. Also, differentiate between strategic planning and operational planning with suitable examples.";
+      }];
+      marks = 6;
+    }
+  ];
+}
