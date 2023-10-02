e8959100ba483e1f6efed8484022f9b90d7ee715
GetPsyched <priyanshutr@proton.me>
2023-10-02 16:01:55 +0530

    qp: add ECPC70 and ECPE71

---
+++ question-papers/ecpc70-ms1-sep-2023.nix
@@ -0,0 +1,64 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 1 EXAMINATION";
+
+  date = { day = 5; month = "Sep"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Microwave Engineering";
+  course.code = "ECPC70";
+
+  semester = 7;
+  pages = 1;
+  time = "50 minutes";
+  marks = 15;
+
+  questions = [
+    {
+      contents = [{
+        text = ''
+          A two-cavity amplifier klystron has the following parameters:
+            - V<sub>0</sub> = 900V, I<sub>0</sub> = 30mA, f = 8GHz
+            - Gap spacing in either cavitiy: d = 1mm
+            - Spacing between centers of cavities: L = 4cm
+            - Effective shunt impedance: R<sub>sh</sub> = 40kΩ
+
+          Given X = 1.841, J<sub>1</sub>(X) = 0.582, and R<sub>0</sub> = V<sub>0</sub>/I<sub>0</sub>, determine:
+            - a. The electron velocity
+            - b. The dc electron transit time
+            - c. The input voltage for maximum output voltage
+            - d. The voltage gain in decibels
+            - e. Efficiency of the amplifier
+        '';
+      }];
+      marks = 4;
+    }
+    {
+      contents = [{
+        text = "Describe the process of velocity modulation in a reflex klystron. Derive the expression for the round trip transit time in the repeller region in terms of the bunching parameter.";
+      }];
+      marks = 4;
+    }
+    {
+      contents = [{
+        text = ''
+          A travelling-wave cube (TWT) operates under the following parameters:
+            - V<sub>0</sub> = 3kV, I<sub>0</sub> = 30mA, f = 10GHz
+            - Characteristic impedance of helix: Z<sub>0</sub> = 10Ω
+            - Circuit length: N = 50
+
+          Given the above, determine:
+            - a. The gain parameter
+            - b. The output power gain `A<sub>p</sub>` in decibels
+            - c. All four propagation constants
+        '';
+      }];
+      marks = 4;
+    }
+    {
+      contents = [{
+        text = "Name some microwave cross field tubes. Describe the principle of operation of a normal cylindrical magnetron and its characterisitics.";
+      }];
+      marks = 3;
+    }
+  ];
+}
---
+++ question-papers/ecpe71-ms1-sep-2023.nix
@@ -0,0 +1,37 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 1 EXAMINATION";
+
+  date = { day = 6; month = "Sep"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Digital Image Processing";
+  course.code = "ECPE71";
+
+  semester = 7;
+  pages = 1;
+  time = "50 minutes";
+  marks = 15;
+
+  questions = [
+    {
+      contents = [{ text = "Explain 2-dimensional (2D) sampling. Also, draw the spectrum of a 2D sampled signal."; }];
+      marks = 4;
+    }
+    {
+      contents = [{
+        text = ''
+          For a 2&times;2 unitary transform `A` and the input image `U`:
+            - a. Check the orthogonality of the matrix `A`
+            - b. Calculate the transformed image `V`
+            - c. Calculate the basis images for this transform
+        '';
+        image = "https://i.imgur.com/mlD7LBy.png";
+      }];
+      marks = "2+2.5+2.5";
+    }
+    {
+      contents = [{ text = "Discuss RGB and CMY colour spaces. Also, explain the conversion from RGB to CMY model and vice-versa."; }];
+      marks = 4;
+    }
+  ];
+}
