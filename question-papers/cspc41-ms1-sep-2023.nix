4b320b67737e65c1a397ea1b0876f8d43019d768
GetPsyched <priyanshutr@proton.me>
2023-10-02 14:45:26 +0530

    qp: add CSPE35 and CSPC41

---
+++ question-papers/cspc41-ms1-sep-2023.nix
@@ -0,0 +1,47 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 1 EXAMINATION";
+
+  date = { day = 4; month = "Sep"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Machine Learning";
+  course.code = "CSPC41";
+
+  semester = 7;
+  pages = 1;
+  time = "50 minutes";
+  marks = 15;
+
+  questions = [
+    {
+      contents = [{
+        text = "What information is conveyed by various boundaries of a Box Plot? What are the formulas to decide its boundaries, lines and how are the outliers identified?";
+      }];
+      marks = 5;
+    }
+    {
+      contents = [{
+        text = "A random sample of 400 families of KKR is found to have an average income of ₹3025. Assume that the population standard deviation is ₹280. An agency claims that the average monthly income of families of KKR is greater than ₹3000. Conduct an appropriate hypothesis test at 95% confidence interval to check the validity of the agency. Design both hypotheses. Mention which test is applied and then compute the validity.";
+      }];
+      marks = 5;
+    }
+    {
+      contents = [{
+        text = ''
+          Consider the following data for PCA based reduction. Compute its Eigen values and Eigen vectors.
+            | Feature | Point 1 | Point 2 | Point 3 | Point 4 |
+            |---------|---------|---------|---------|---------|
+            | X1      | 5       | 10      | 2       | 4       |
+            | X2      | 8       | 12      | 6       | 7       |
+        '';
+      }];
+      marks = 5;
+    }
+    {
+      contents = [{
+        text = "Consider X = {2, 4, 6, 10, 12} and Y = {5, 7, 15, 18, 25}. Step by step, estimate its coefficients (b<sub>0</sub> and b<sub>1</sub>) for simple linear regression using Method 2 (using SS<sub>XY</sub> and SS<sub>XX</sub>)";
+      }];
+      marks = 5;
+    }
+  ];
+}
---
+++ question-papers/cspe35-ms2-nov-2022.nix
@@ -0,0 +1,44 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 2 EXAMINATION";
+
+  date = { day = 19; month = "Nov"; year = 2022; };
+  programme = "B. Tech.";
+  course.name = "Mobile Application Development";
+  course.code = "CSPE35";
+
+  semester = 5;
+  pages = 1;
+  time = "50 minutes";
+  marks = 15;
+
+  questions = [
+    {
+      contents = [{
+        text = "Write the code to access an audio and a video resource file in multimedia handling in Android Studio.";
+        prompt = "Assume file location / directory as per requirement";
+      }];
+      marks = 2;
+    }
+    {
+      contents = [{ text = "What is the role of Intent in Android Studio? Describe the types of Intents with examples for each."; }];
+      marks = 3;
+    }
+    {
+      contents = [
+        {
+          text = "Describe the ARM architecture and its three architecture profiles briefly. Differentiate between RISC and CISC instruction sets in Android Application Development.";
+        }
+        {
+          text = "Construct a navigation bar created for a coffee shop application in Android Studio. Write XML code for the GUI of your design. Assume variables as per your choice. Draw the rough diagram / view of the GUI, which will be formed by the XML code. Also, write the corresponding Java code for the “Place Order” button which performs the required order completion request after adding the coffees in the cart.";
+          prompt = "Write only the main parts of the code";
+        }
+      ];
+      marks = 5;
+    }
+    {
+      contents = [{ text = "Explain service life-cycle with a block diagram. Also describe `startService()` and `bindService()` callback methods."; }];
+      marks = 5;
+    }
+  ];
+}
