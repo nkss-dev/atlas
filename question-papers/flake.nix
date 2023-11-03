71be1e9da675b50cfaa9560a0aa0fd00a8414297
GetPsyched <priyanshu@getpsyched.dev>
2023-11-03 23:28:20 +0530

    qp: feat: use nix

---
+++ question-papers/flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Just for syntax highlighting ATM";
+
+  inputs = {
+    nix-env.url = "github:GetPsyched/nix-starter-flakes?dir=nix";
+    nix-env.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = inputs@{ nixpkgs, nix-env, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      nix-env-pkgs = nix-env.outputs.packages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = with nix-env-pkgs; [ default vscode ];
+      };
+    };
+}
