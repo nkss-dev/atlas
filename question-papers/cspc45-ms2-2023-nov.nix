18e0483fb253cef07d460165cdab0deff6a92408
GetPsyched <priyanshu@getpsyched.dev>
2023-11-04 13:48:31 +0530

    qp: feat: add cspc45 and itpc45 papers

---
+++ question-papers/cspc45-ms2-2023-nov.nix
@@ -0,0 +1,45 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 2 EXAMINATION";
+
+  date = { day = 4; month = "Nov"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Information Security";
+  course.code = "CSPC45";
+
+  semester = 7;
+  pages = 1;
+  time = "50 minutes";
+
+  questions = [
+    {
+      contents = [{
+        text = ''
+          Using the RSA algorithm, find `n`, and `d` if `p = 11`, `q = 3`,
+          `e = 3`. Encrypt the message `Hello World` using 00 to 25 for letters
+          A to Z and 26 for the space.
+        '';
+      }];
+      marks = 6;
+    }
+    {
+      contents = [{
+        text = ''
+          What is diffusion and confusion in modern symmetric-key block
+          ciphers? How do product ciphers achieve diffusion and confusion?
+          Explain with the help of an example.
+        '';
+      }];
+      marks = 5;
+    }
+    {
+      contents = [{
+        text = ''
+          Explain the final design of a Feistel cipher. Also, discuss the idea
+          of using non-convertible components in the Feistel cipher.
+        '';
+      }];
+      marks = 4;
+    }
+  ];
+}
---
+++ question-papers/itpc45-ms2-2023-nov.nix
@@ -0,0 +1,34 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 2 EXAMINATION";
+
+  date = { day = 4; month = "Nov"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Advanced Data Structures and Algorithms";
+  course.code = "ITPC45";
+
+  semester = 7;
+  pages = 1;
+  time = "50 minutes";
+
+  questions = [
+    {
+      contents = [{
+        text = ''
+          Write the algorithm for decrease key operation in a binomial heap.
+          Also, discuss with a suitable example.
+        '';
+      }];
+      marks = 7.5;
+    }
+    {
+      contents = [{
+        text = ''
+          Write the algorithm for delete operation in Fibonacci Heap. Also,
+          discuss the amortised cost.
+        '';
+      }];
+      marks = 7.5;
+    }
+  ];
+}
