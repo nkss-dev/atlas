4b1300d14d62cc32355d7e81debf95cb9dce7f0a
GetPsyched <priyanshu@getpsyched.dev>
2023-11-04 13:39:41 +0530

    fix: xdg-open might not work on other operating systems

--- question-papers/print.sh
+++ question-papers/print.sh
@@ -7,4 +7,11 @@ curl "https://printer.up.railway.app/html" \
     --url-query target=question-paper \
     --json '{ "options": { "tailwindcss": true }, "data": '"$DATA"' }' \
     1>.auto-gen/$1.html
-xdg-open .auto-gen/$1.html
+
+if [[ "$OSTYPE" == "linux-gnu"* || "$OSTYPE" == "freebsd"* ]]; then
+    xdg-open .auto-gen/$1.html
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    open .auto-gen/$1.html
+else
+    echo "The generated HTML could not be opened automatically. It's located here: \`.auto-gen/$1.html\`"
+fi
