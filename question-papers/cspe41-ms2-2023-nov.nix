0dc944da631eaf42f2bfab919991154432a8a96a
GetPsyched <priyanshu@getpsyched.dev>
2023-11-05 17:27:58 +0530

    qp: feat: last of CS ms2

---
+++ question-papers/cspe41-ms2-2023-nov.nix
@@ -0,0 +1,47 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 2 EXAMINATION";
+
+  date = { day = 4; month = "Nov"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Cloud and Grid Computing";
+  course.code = "CSPE41";
+
+  semester = 7;
+  pages = 1;
+  time = "50 minutes";
+
+  questions = [
+    {
+      contents = [{
+        text = ''
+          Differentiate between full and para virtualisation. Outline the
+          problems in virtualising in CPU, I/O, and memory devices and suggest
+          how it could be overridden for efficient utilisation of cloud
+          services.
+        '';
+      }];
+      marks = 5;
+    }
+    {
+      contents = [{
+        text = ''
+          What is an SLA? Explain how an SLA is important for an organisation.
+          What are the important steps that need to be followed in preparing
+          SLAs?
+        '';
+      }];
+      marks = 5;
+    }
+    {
+      contents = [{
+        text = ''
+          1. Explain various migration techniques used in virtual machine
+             migration.
+          2. Explain the architecture of a federated cloud.
+        '';
+      }];
+      marks = "3+2";
+    }
+  ];
+}
