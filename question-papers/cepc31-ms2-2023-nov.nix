fa513d6e90676ca7d5f51390c4ba1fe5b9f9b2fc
GetPsyched <priyanshu@getpsyched.dev>
2023-11-06 23:18:22 +0530

    style: format document

--- question-papers/cepc31-ms2-2023-nov.nix
+++ question-papers/cepc31-ms2-2023-nov.nix
@@ -14,19 +14,27 @@
   questions = [
     [
       [{
-        text = "Design a circular water tank resting on ground, having capacity of 2,00,000 litres of water using approximate method. Assume concrete of grade M25 and steel of Fe500 ";
+        text = ''
+          Design a circular water tank resting on ground, having capacity of
+          2,00,000 litres of water using approximate method. Assume concrete
+          of grade M25 and steel of Fe500.
+        '';
         marks = 12;
       }]
     ]
-
     [
       [{
-        text = 
-          "A concrete beam of dimension 100mm X 300mm is pre-tensioned with 5 straight wires of 7mm diameter over its complete length of 6m. The anchorage slip of 5mm is observed. The average prestress after short-term losses is 0.7fpk = 1200N/mm^2 and the age of loading is given as 28 days. Given that Ep = 200 X 10^3 Mpa , Ec = 35000 Mpa, relative humidity as 80%. Determine the losses of prestress. Neglect the weight of the beam in the computation of the stresses"
-        ;
+        text = ''
+          A concrete beam of dimension 100mm X 300mm is pre-tensioned with 5
+          straight wires of 7mm diameter over its complete length of 6m. The
+          anchorage slip of 5mm is observed. The average prestress after
+          short-term losses is 0.7fpk = 1200N/mm^2 and the age of loading is
+          given as 28 days. Given that Ep = 200 X 10^3 Mpa , Ec = 35000 Mpa,
+          relative humidity as 80%. Determine the losses of prestress. Neglect
+          the weight of the beam in the computation of the stresses
+        '';
         marks = 18;
       }]
     ]
-
   ];
 }
