e88b68c066f630dfd5862c70f5d4455144e73409
Akarshit Singh <94007062+AKCodeFanatic@users.noreply.github.com>
2023-11-06 23:52:52 +0530

    qp: feat: add cepc31-ms2-2023-nov (#27)
    Co-authored-by: GetPsyched <priyanshu@getpsyched.dev>

---
+++ question-papers/cepc31-ms2-2023-nov.nix
@@ -0,0 +1,41 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 2 EXAMINATION";
+
+  date = { day = 2; month = "Nov"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Design of Concrete Structures - II";
+  course.code = "CEPC31";
+
+  semester = 5;
+  pages = 1;
+  time = "60 minutes";
+
+  questions = [
+    [
+      [{
+        text = ''
+          Design a circular water tank resting on the ground, having a capacity
+          of 2,00,000 litres of water using approximate method. Assume concrete
+          of grade M25 and steel of Fe500.
+        '';
+        marks = 12;
+      }]
+    ]
+    [
+      [{
+        text = ''
+          A concrete beam of dimensions 100mm×300mm is pre-tensioned with 5
+          straight wires of 7mm diameter over its complete length of 6m. An
+          anchorage slip of 5mm is observed. The average prestress after
+          short-term losses is 0.7fpk = 1200N/mm<sup>2</sup> and the age of
+          loading is given as 28 days. Given that E<sub>p</sub> = 200 ×
+          10<sup>3</sup> MPa, E<sub>c</sub> = 35000 MPa, relative humidity as
+          80%. Determine the losses of prestress. Neglect the weight of the beam
+          in the computation of the stresses.
+        '';
+        marks = 18;
+      }]
+    ]
+  ];
+}
