12e8b05104d868c62ee523d6d661a6426a90dcbb
GetPsyched <priyanshu@getpsyched.dev>
2023-11-06 23:19:52 +0530

    fix: grammar + question-specific styling

--- question-papers/cepc31-ms2-2023-nov.nix
+++ question-papers/cepc31-ms2-2023-nov.nix
@@ -4,7 +4,7 @@
 
   date = { day = 2; month = "Nov"; year = 2023; };
   programme = "B. Tech.";
-  course.name = "Design of Concrete Structures 2";
+  course.name = "Design of Concrete Structures - II";
   course.code = "CEPC31";
 
   semester = 5;
@@ -15,8 +15,8 @@
     [
       [{
         text = ''
-          Design a circular water tank resting on ground, having capacity of
-          2,00,000 litres of water using approximate method. Assume concrete
+          Design a circular water tank resting on the ground, having a capacity
+          of 2,00,000 litres of water using approximate method. Assume concrete
           of grade M25 and steel of Fe500.
         '';
         marks = 12;
@@ -25,13 +25,14 @@
     [
       [{
         text = ''
-          A concrete beam of dimension 100mm X 300mm is pre-tensioned with 5
-          straight wires of 7mm diameter over its complete length of 6m. The
+          A concrete beam of dimensions 100mm×300mm is pre-tensioned with 5
+          straight wires of 7mm diameter over its complete length of 6m. An
           anchorage slip of 5mm is observed. The average prestress after
-          short-term losses is 0.7fpk = 1200N/mm^2 and the age of loading is
-          given as 28 days. Given that Ep = 200 X 10^3 Mpa , Ec = 35000 Mpa,
-          relative humidity as 80%. Determine the losses of prestress. Neglect
-          the weight of the beam in the computation of the stresses
+          short-term losses is 0.7fpk = 1200N/mm<sup>2</sup> and the age of
+          loading is given as 28 days. Given that E<sub>p</sub> = 200 ×
+          10<sup>3</sup> MPa, E<sub>c</sub> = 35000 MPa, relative humidity as
+          80%. Determine the losses of prestress. Neglect the weight of the beam
+          in the computation of the stresses.
         '';
         marks = 18;
       }]
