951e3232a1b99a0a938efd1f2299f5960e18880b
GetPsyched <priyanshu@getpsyched.dev>
2023-11-06 11:48:53 +0530

    fix: remove comments and move to newer format

--- question-papers/cspc27-ms2-2023-nov.nix
+++ question-papers/cspc27-ms2-2023-nov.nix
@@ -1,60 +1,52 @@
 {
-  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA"; # this value remains unchanged
-  subtitle = "MID TERM - II EXAMINATION"; # this value can have `MID TERM - X` or `END TERM`
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - II EXAMINATION";
 
-  date = { day = 4; month = "Nov"; year = 2023; }; # the date of the examination
-  programme = "B. Tech."; # this value remains unchanged [tm]
-  course.name = "Software Engineering"; # the full course name of the course
-  course.code = "CSPC27"; # the course code of the course
+  date = { day = 4; month = "Nov"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Software Engineering";
+  course.code = "CSPC27";
 
-  semester = 3; # the semester of the batch for which the exam was given
-  pages = 1; # total number of pages
-  time = "50 minutes"; # time given
+  semester = 3;
+  pages = 1;
+  time = "50 minutes";
 
-  # the array containing all the questions
   questions = [
-    {
-      # contents can contain multiple of the below objects which, if any, will be resolved as OR questions.
-      contents = [{
-        text = "Differentiate between activity diagram and sequence diagram"; # this attribute supports markdown formatting
-      }];
-      marks = 2.5; # usually a number, but can also be a string sum of marks (eg. "2+2.5+2.5")
-    },
-    {
-      # contents can contain multiple of the below objects which, if any, will be resolved as OR questions.
-      contents = [{
-        text = "Differentiate between weak and strong entity set. Explain with an example."; # this attribute supports markdown formatting
-      }];
-      marks = 2.5; # usually a number, but can also be a string sum of marks (eg. "2+2.5+2.5")
-    },
-    {
-      # contents can contain multiple of the below objects which, if any, will be resolved as OR questions.
-      contents = [{
-        text = "What are features of SRS? What are User Interface Requirements?"; # this attribute supports markdown formatting
-      }];
-      marks = 2.5; # usually a number, but can also be a string sum of marks (eg. "2+2.5+2.5")
-    },
-    {
-      # contents can contain multiple of the below objects which, if any, will be resolved as OR questions.
-      contents = [{
-        text = "Differentiate between Behavioral and non-Behavioral requirements."; # this attribute supports markdown formatting
-      }];
-      marks = 2.5; # usually a number, but can also be a string sum of marks (eg. "2+2.5+2.5")
-    },
-    {
-      # contents can contain multiple of the below objects which, if any, will be resolved as OR questions.
-      contents = [{
-        text = "Draw DFD diagram for library management system. Explain in detail."; # this attribute supports markdown formatting
-      }];
-      marks = 5; # usually a number, but can also be a string sum of marks (eg. "2+2.5+2.5")
-    },
-    {
-      # contents can contain multiple of the below objects which, if any, will be resolved as OR questions.
-      contents = [{
-        text = "Differentiate between Class diagram and Use case diagram"; # this attribute supports markdown formatting
-      }];
-      marks = 2.5; # usually a number, but can also be a string sum of marks (eg. "2+2.5+2.5")
-    }
-    ...
+    [
+      [{
+        text = "Differentiate between activity diagram and sequence diagram";
+        marks = 2.5;
+      }]
+    ]
+    [
+      [{
+        text = "Differentiate between weak and strong entity set. Explain with an example.";
+        marks = 2.5;
+      }]
+    ]
+    [
+      [{
+        text = "What are features of SRS? What are User Interface Requirements?";
+        marks = 2.5;
+      }]
+    ]
+    [
+      [{
+        text = "Differentiate between Behavioral and non-Behavioral requirements.";
+        marks = 2.5;
+      }]
+    ]
+    [
+      [{
+        text = "Draw DFD diagram for library management system. Explain in detail.";
+        marks = 5;
+      }]
+    ]
+    [
+      [{
+        text = "Differentiate between Class diagram and Use case diagram";
+        marks = 2.5;
+      }]
+    ]
   ];
 }
