c6e231371c2c88ce8c95d15444aadbc27243f7ce
Vinay <149948922+ProtonStar12@users.noreply.github.com>
2023-11-06 22:13:07 +0530

    Create eepc202-ms1-2023-sep-nov.nix
    first paper

---
+++ question-papers/eepc202-ms1-2023-sep-nov.nix
@@ -0,0 +1,40 @@
+{
+  title = "NATIONAL INSTITUE OF TECHNOLOGY, KURUKSHETRA";
+  subtile = "MID TERM - 2 EXAMINATION";
+
+  date = { day = 4; month = "Nov"; year = 2023; };
+  programme = "B.Tech.";
+  course.name = "Analog and Digital Electronics";
+  course.code = "EEPC202";
+
+  semester = 3;
+  pages = 1;
+  time = "50 minutes";
+
+  questions = [
+  {
+    contents = [{
+      text = ''
+       Obtain the dc conditions for voltage-divider bias circuit for a CE-BJT amplifier and give design constraints along with the remark on stability of Q-       Point.
+       '';
+    }];
+    marks = 5;
+  }
+  {
+    contents = [{
+      text = ''
+      Draw the Re-equivalent model circuit for a typical common-emitter Fixed-Bias configuration and derive expression for Av, Zn and Zo (the absence of the      resistance ro).
+      '';
+    }];
+    marks = 5;
+  }
+  {
+    contents = [{
+      text = ''
+      For Emitter bias configuration Vcc = 20V, Rb = 430, Rc = 2k, Re = 1k , beta = 50. Find Ib,Vce,Vc,Ve,Vb,Vbc.
+      '';
+    }];
+    marks = 5;
+  }
+  ];
+}
