e551ba631a0abd133ecf5bed4c32b597dda1b418
Akarshit <akarshitsingh1234@gmail.com>
2023-11-06 21:57:27 +0530

    Adding the Question Paper of CEPC31 , MIDSEM2,NOVEMBER,2023.

---
+++ question-papers/cepc31-ms2-2023-nov.nix
@@ -0,0 +1,32 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 2 EXAMINATION";
+
+  date = { day = 2; month = "Nov"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Design of Concrete Structures 2";
+  course.code = "CEPC31";
+
+  semester = 5;
+  pages = 1;
+  time = "60 minutes";
+
+  questions = [
+    [
+      [{
+        text = "Design a circular water tank resting on ground, having capacity of 2,00,000 litres of water using approximate method. Assume concrete of grade M25 and steel of Fe500 ";
+        marks = 12;
+      }]
+    ]
+
+    [
+      [{
+        text = 
+          "A concrete beam of dimension 100mm X 300mm is pre-tensioned with 5 straight wires of 7mm diameter over its complete length of 6m. The anchorage slip of 5mm is observed. The average prestress after short-term losses is 0.7fpk = 1200N/mm^2 and the age of loading is given as 28 days. Given that Ep = 200 X 10^3 Mpa , Ec = 35000 Mpa, relative humidity as 80%. Determine the losses of prestress. Neglect the weight of the beam in the computation of the stresses"
+        ;
+        marks = 18;
+      }]
+    ]
+
+  ];
+}
