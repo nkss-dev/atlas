2fd1c77025f873805dcde1cfb47cd9d3b3480219
GetPsyched <priyanshu@getpsyched.dev>
2023-11-07 22:05:51 +0530

    fix: update old format

--- question-papers/eepc202-ms1-2023-sep.nix
+++ question-papers/eepc202-ms1-2023-sep.nix
@@ -12,29 +12,29 @@
   time = "50 minutes";
 
   questions = [
-    {
-      contents = [{
+    [
+      [{
         text = ''
           Obtain the dc conditions for voltage-divider bias circuit for a CE-BJT amplifier and give design constraints along with the remark on stability of Q-       Point.
         '';
-      }];
-      marks = 5;
-    }
-    {
-      contents = [{
+        marks = 5;
+      }]
+    ]
+    [
+      [{
         text = ''
           Draw the Re-equivalent model circuit for a typical common-emitter Fixed-Bias configuration and derive expression for Av, Zn and Zo (the absence of the      resistance ro).
         '';
-      }];
-      marks = 5;
-    }
-    {
-      contents = [{
+        marks = 5;
+      }]
+    ]
+    [
+      [{
         text = ''
           For Emitter bias configuration Vcc = 20V, Rb = 430, Rc = 2k, Re = 1k , beta = 50. Find Ib,Vce,Vc,Ve,Vb,Vbc.
         '';
-      }];
-      marks = 5;
-    }
+        marks = 5;
+      }]
+    ]
   ];
 }
