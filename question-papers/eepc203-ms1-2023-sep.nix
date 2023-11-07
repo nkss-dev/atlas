627745a5fb2c13204796ab4a4fe1b0aea76088a6
GetPsyched <priyanshu@getpsyched.dev>
2023-11-07 22:04:55 +0530

    style: run formatter

--- question-papers/eepc203-ms1-2023-sep.nix
+++ question-papers/eepc203-ms1-2023-sep.nix
@@ -2,7 +2,7 @@
   title = "NATIONAL INSTITUTE OF TECHNOLOGY , KURUKSHETRA";
   subtitle = "MID TERM - 1 EXAMINATION";
 
-  date = { day = 5; month = "Sep" ; year = 2023; };
+  date = { day = 5; month = "Sep"; year = 2023; };
   programme = "B. Tech.";
   cousre.name = "Generation Transmission and Distribution";
   cousre.code = "EEPC203";
@@ -12,38 +12,38 @@
   time = "50 minutes";
 
   questions = [
-  [
-    [{
-      text = ''
-      Draw the layout of thermal power plant
-      '';
-      marks = 5;
-    }]
-  ]
-  [
-    [{
-      text = ''
-      Derive the expression foe inductance of transposed , 3-phased asymmetrically spaced triangular arrangement.
-      '';
-      marks = 5;
-    }]
-  ]
-  [
-    [{
-      text = ''
-      Explain skin and proximity effect.
-      '';
-      marks = 2;
-    }]
-  ]
-  [
-    [{
-      text = ''
-      Calculate per phase per kilometre inductance of a bundled conductor trasmission line given below.
-      '';
-      image = "https://imgur.com/a/8KH8XhB";
-    }]
-  ]
- ]; 
+    [
+      [{
+        text = ''
+          Draw the layout of thermal power plant
+        '';
+        marks = 5;
+      }]
+    ]
+    [
+      [{
+        text = ''
+          Derive the expression foe inductance of transposed , 3-phased asymmetrically spaced triangular arrangement.
+        '';
+        marks = 5;
+      }]
+    ]
+    [
+      [{
+        text = ''
+          Explain skin and proximity effect.
+        '';
+        marks = 2;
+      }]
+    ]
+    [
+      [{
+        text = ''
+          Calculate per phase per kilometre inductance of a bundled conductor trasmission line given below.
+        '';
+        image = "https://imgur.com/a/8KH8XhB";
+      }]
+    ]
+  ];
 
 }
