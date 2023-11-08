8f4357a6da49fc86803adb75302a87d2d9b0f581
Vinay <149948922+ProtonStar12@users.noreply.github.com>
2023-11-08 15:22:05 +0530

    qp: add eepc202 and eepc203 (#28)
    Co-authored-by: GetPsyched <priyanshu@getpsyched.dev>

---
+++ question-papers/eepc203-ms1-2023-sep.nix
@@ -0,0 +1,47 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 1 EXAMINATION";
+
+  date = { day = 5; month = "Sep"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Generation, Transmission and Distribution";
+  course.code = "EEPC203";
+
+  semester = 3;
+  pages = 1;
+  time = "50 minutes";
+
+  questions = [
+    [
+      [{
+        text = "Draw the layout of thermal power plant.";
+        marks = 5;
+      }]
+    ]
+    [
+      [{
+        text = ''
+          Derive the expression for the inductance of transposed, 3-phased
+          asymmetrically spaced triangular arrangement.
+        '';
+        marks = 5;
+      }]
+    ]
+    [
+      [{
+        text = "Explain skin and proximity effect.";
+        marks = 2;
+      }]
+    ]
+    [
+      [{
+        text = ''
+          Calculate per phase per kilometer inductance of a bundled conductor
+          trasmission line given below.
+        '';
+        marks = 3;
+        image = "https://i.imgur.com/8fOlHiW.jpeg";
+      }]
+    ]
+  ];
+}
