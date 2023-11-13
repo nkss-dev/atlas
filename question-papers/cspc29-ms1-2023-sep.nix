60066133b963ffbad2be85430e73a197b213206d
GetPsyched <priyanshu@getpsyched.dev>
2023-11-13 23:57:02 +0530

    style: run formatter

--- question-papers/cspc29-ms1-2023-sep.nix
+++ question-papers/cspc29-ms1-2023-sep.nix
@@ -1,44 +1,44 @@
 {
   title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
-  subtitle = "MID TERM - I EXAMINATION"; 
+  subtitle = "MID TERM - I EXAMINATION";
 
-  date = { day = 04; month = "Sep"; year = 2023; }; 
-  programme = "B. Tech."; 
-  course.name = "Computer Organization And Architecture"; 
-  course.code = "CSPC29"; 
+  date = { day = 04; month = "Sep"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Computer Organization And Architecture";
+  course.code = "CSPC29";
 
-  semester = 3; 
-  pages = 1; 
-  time = "50 minutes"; 
+  semester = 3;
+  pages = 1;
+  time = "50 minutes";
 
   questions = [
     [
       [
         {
           text = "What is the difference between a direct and an indirect address instruction? How many references are needed for each type of instruction to bring an operand in to a process register?";
-          marks = 5; 
+          marks = 5;
         }
       ]
     ]
     [
-        [
-            {
-                text = "A digital common bus system for 16 registers of 32 bits each. The bus is constructed with multiplexers.   
-                a. How many selection inputs are there in each multiplexer?  
-                b. What size of multiplexers are needed?  
+      [
+        {
+          text = "A digital common bus system for 16 registers of 32 bits each. The bus is constructed with multiplexers.
+                a. How many selection inputs are there in each multiplexer?
+                b. What size of multiplexers are needed?
                 c. How many multiplexers are there in the bus?";
-                marks = 5;
-            }
-        ]
+          marks = 5;
+        }
+      ]
     ]
     [
-        [
-            {
-                text = "Simplify the following Boolean function in sum-of-products form by means of a four-variable map. Draw the logic diagram with (a) AND-OR gate; (b) NAND gates  
+      [
+        {
+          text = "Simplify the following Boolean function in sum-of-products form by means of a four-variable map. Draw the logic diagram with (a) AND-OR gate; (b) NAND gates
                 F(A, B, C, D) = Σ(0, 2, 8, 9, 10, 11, 14, 15)";
-                marks = 5;
-            }
-        ]
+          marks = 5;
+        }
+      ]
     ]
   ];
 }
