f955284ee58e7c57f15a821bc0357131f9e4fdd0
GetPsyched <priyanshu@getpsyched.dev>
2023-11-14 00:03:19 +0530

    fix: enforce max line width and fix grammar

--- question-papers/cspc29-ms1-2023-sep.nix
+++ question-papers/cspc29-ms1-2023-sep.nix
@@ -1,10 +1,10 @@
 {
   title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
-  subtitle = "MID TERM - I EXAMINATION";
+  subtitle = "MID TERM - 1 EXAMINATION";
 
   date = { day = 04; month = "Sep"; year = 2023; };
   programme = "B. Tech.";
-  course.name = "Computer Organization And Architecture";
+  course.name = "Computer Organization and Architecture";
   course.code = "CSPC29";
 
   semester = 3;
@@ -15,7 +15,11 @@
     [
       [
         {
-          text = "What is the difference between a direct and an indirect address instruction? How many references are needed for each type of instruction to bring an operand in to a process register?";
+          text = ''
+            What is the difference between a direct and an indirect address
+            instruction? How many references are needed for each type of
+            instruction to bring an operand in to a process register?
+          '';
           marks = 5;
         }
       ]
@@ -23,10 +27,13 @@
     [
       [
         {
-          text = "A digital common bus system for 16 registers of 32 bits each. The bus is constructed with multiplexers.
-                a. How many selection inputs are there in each multiplexer?
-                b. What size of multiplexers are needed?
-                c. How many multiplexers are there in the bus?";
+          text = ''
+            A digital common bus system for 16 registers of 32 bits each. The
+            bus is constructed with multiplexers.
+              1. How many selection inputs are there in each multiplexer?
+              2. What size of multiplexers are needed?
+              3. How many multiplexers are there in the bus?
+          '';
           marks = 5;
         }
       ]
@@ -34,8 +41,14 @@
     [
       [
         {
-          text = "Simplify the following Boolean function in sum-of-products form by means of a four-variable map. Draw the logic diagram with (a) AND-OR gate; (b) NAND gates
-                F(A, B, C, D) = Σ(0, 2, 8, 9, 10, 11, 14, 15)";
+          text = ''
+            Simplify the following Boolean function in sum-of-products form by
+            means of a four-variable map. Draw the logic diagram with:
+              1. AND-OR gate
+              2. NAND gates
+
+            F(A, B, C, D) = Σ(0, 2, 8, 9, 10, 11, 14, 15)
+          '';
           marks = 5;
         }
       ]
