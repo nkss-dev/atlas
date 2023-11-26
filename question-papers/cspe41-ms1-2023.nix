99f18056d3d219cee11413f1f1f9342ee17832fb
GetPsyched <priyanshu@getpsyched.dev>
2023-11-26 18:37:27 +0530

    qp: feat: add missing cspe41-ms1-2023

---
+++ question-papers/cspe41-ms1-2023.nix
@@ -0,0 +1,62 @@
+{
+  title = "NATIONAL INSTITUTE OF TECHNOLOGY, KURUKSHETRA";
+  subtitle = "MID TERM - 1 EXAMINATION";
+
+  date = { day = 6; month = "Sep"; year = 2023; };
+  programme = "B. Tech.";
+  course.name = "Cloud and Grid Computing";
+  course.code = "CSPE41";
+
+  semester = 7;
+  pages = 1;
+  time = "50 minutes";
+
+  questions = [
+    [
+      [
+        {
+          text = ''
+            Differentiate between scalability and elasticity in cloud computing.
+          '';
+          marks = 1;
+        }
+        {
+          text = ''
+            Explain how cloud computing is different from outsourcing and
+            provision of application services.
+          '';
+          marks = 2;
+        }
+        {
+          text = "What are the three forms of lease?";
+          marks = 2;
+        }
+      ]
+    ]
+    [
+      [{
+        text = ''
+          Explain the fundamentals of cloud computing architecture using a neat
+          diagram and also explain the challenges faced by cloud computing
+          architecture.
+        '';
+        marks = 5;
+      }]
+    ]
+    [
+      [{
+        text = ''
+          Explain the features of the following:
+            - Infrastructure as a service
+            - Database as a service
+            - Function as a service
+            - Communication as a service
+
+          What factors should you consider while selecting a cloud service
+          provider?
+        '';
+        marks = 5;
+      }]
+    ]
+  ];
+}
